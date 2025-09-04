b594054b1e95acd7add6a3d8a45080d7d8854868
sabik-ahmd <sabik.mh@gmail.com>
2025-09-04 21:40:55 +0530

    Updated gallery with upload & delete option

---
+++ netlify/functions/upload.jsx
@@ -0,0 +1,25 @@
+import fetch from "node-fetch";
+
+export async function handler(event) {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  const body = JSON.parse(event.body);
+  const { file } = body;
+
+  // Use Cloudinary unsigned upload preset OR signed upload
+  const url = `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/auto/upload`;
+
+  const res = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      file,
+      upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const data = await res.json();
+  return { statusCode: 200, body: JSON.stringify(data) };
+}
